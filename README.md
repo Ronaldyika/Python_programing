03697a1dc28c0559b71747b362f15be2063420e1
Ronaldyika <buhnyuyronald@gmail.com>
2022-11-08 04:01:54 +0100

    algorithmn selection

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 python learning . from zero to hero
     #daily routin
 spend atleast 2 hrs daily on python
+1)take a project on completion
---
+++ highest.py
@@ -0,0 +1,6 @@
+num1 = str(input("enter num1: "))
+num2 = str(input("enter num2: "))
+if num1> num2:
+    print(num1 + " is the highest number")
+else:
+    print(num2 + " is the greatest number")
