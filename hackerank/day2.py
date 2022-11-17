1986e23eb79b0050d30879146ff5d9c819b6ea82
Ronaldyika <buhnyuyronald@gmail.com>
2022-11-17 23:11:36 +0100

    day2

---
+++ hackerank/day2.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    # Write your code here
+    result = [1 if x > y else -1 if y > x else 0 for x,y in 
+    list(zip(a,b))]
+    print(result)
+    return result.count(1),result.count(-1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
