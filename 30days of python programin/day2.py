88b6244ebcf51f60ed7ab2f8a4e1a1a97f269cc3
Ronaldyika <buhnyuyronald@gmail.com>
2023-03-22 20:38:01 +0100

    more on strings and user input

---
+++ 30days of python programin/day2.py
@@ -0,0 +1,10 @@
+#understanding traceback errors and reading where the code error is located in python terminal
+
+#1 = getting user input
+
+name = input("what is your first name: ")
+age = int(input('input your age: '))
+
+detail = ("my name is "+ name  + " and i am "+str(age)+ "years old");
+
+print(detail)
