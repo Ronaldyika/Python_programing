e5dc45951d6f9c3a31f3036641a75edc8dd7315c
Ronaldyika <buhnyuyronald@gmail.com>
2023-03-25 04:36:42 +0100

    zip

--- 30days of python programin/day9.py
+++ 30days of python programin/day9.py
@@ -5,3 +5,11 @@ counter = 1;
 
 for counter, name in enumerate(list, start=1):
     print(f'{counter}.{name}')
+
+pets = ['dogs','cats','ducks','rabits']
+
+owner = ['men','women','family','farmer']
+
+
+belongs_to = zip(owner,pets)
+print((belongs_to))
