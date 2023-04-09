6befd5ab17f25bb2f90c03a77f620011ec6a7cc4
Ronaldyika <buhnyuyronald@gmail.com>
2023-04-09 19:20:07 +0100

    adding contects to the block

--- ronpostscribers/blog/admin.py
+++ ronpostscribers/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import postmodel
 
 # Register your models here.
+
+class detialseen(admin.ModelAdmin):
+    list_display = ('title','datecreated')
+admin.site.register(postmodel)
--- ronpostscribers/blog/models.py
+++ ronpostscribers/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class postmodel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    datecreated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
